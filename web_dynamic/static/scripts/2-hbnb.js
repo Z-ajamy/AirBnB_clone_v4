bd279e5ccb7ca73e4387bea4a1be3d59b3deaf0a
Z-ajamy <abdorahman0283@gmail.com>
2024-08-11 18:19:52 +0300

    0

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    const selectedAmenities = {};
+    $('input[type="checkbox"]').change(function () {
+      if ($(this).is(':checked')) {
+        selectedAmenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete selectedAmenities[$(this).data('id')];
+      }
+      $('.amenities h4').text(Object.values(selectedAmenities).join(', '));
+    });
+  
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    });
+  });
+  
